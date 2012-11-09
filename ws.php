b4be1d68b873d45deebe96c72163d62b36ba37d5
Martin Goldhahn <mgoldhahn@gmail.com>
2012-11-09 09:44:24 +0100

    deleted submodule for php-wsdl-generator

--- ws.php
+++ ws.php
@@ -4,8 +4,8 @@ include_once __DIR__ .'/ws/WSDispatcher.php';
 $mappings = array(
 		'RepositoryService' => array('class' => 'RepositoryServiceImpl', 'file' => __DIR__.'/impl/RepositoryServiceImpl.php'),
 );
-
-$dispatcher = new WSDispatcher('wsdl/CMISWS-Service.wsdl', $mappings, "http://{$_SERVER['HTTP_HOST']}:{$_SERVER['SERVER_PORT']}{$_SERVER['SCRIPT_NAME']}");
+$baseUrl = "http://{$_SERVER['HTTP_HOST']}:{$_SERVER['SERVER_PORT']}{$_SERVER['SCRIPT_NAME']}";
+$dispatcher = new WSDispatcher('wsdl/CMISWS-Service.wsdl', $mappings, $baseUrl);
 
 $dispatcher->dispatch();
 
